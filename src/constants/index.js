23593623625f0901c9ca3fe785473dc01cc6601c
50ajmalkhan <ajmalkhan50500@gmail.com>
2023-09-02 23:59:57 +0500

    CWHA-0001 Implement portfoli for myself

---
+++ src/constants/index.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "GiddyOps Laundry System",
+    desc: "Developed call center functionality using React and Node.js for the Giddyops project, resulting in eﬃcient call routing and management within the organization. Speciﬁcally, my work on the project involved designing and implementing a range of features to improve call center operations, including call routing and management, automated call distribution, and call tracking and reporting. To achieve this, I used a combination of React and Node.js components and libraries, such as Redux for state management and Twilio for call handling and messaging.",
+    tags: [
+      {
+        name: "React js",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+      {
+        name: "Node js",
+        color: "yellow-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109025615442608168/pvz.png",
+    code: "https://github.com/50ajmalkhan",
+  },
+  {
+    title: "Connect",
+    desc: "Worked on the Connect project, which enabled remote workers to communicate via calling and messaging while also providing real-time status updates on their availability. I used a range of technologies, including Next js and Node.js for the front - end and back - end development, respectively.I also used a variety of libraries and tools, such as Socket.io for real - timecommunication and Firebase for user authentication and data storage.",
+tags: [
+  {
+    name: "NextJs",
+    color: "red-600",
+  },
+  {
+    name: "Socket.io",
+    color: "green-600",
+  },
+  {
+    name: "Node js",
+    color: "yellow-600",
+  },
+  {
+    name: "MongoDB",
+    color: "blue-800",
+  },
+],
+  img: "https://user-images.githubusercontent.com/88134306/227576773-06c3a1d2-0a1a-4ab2-8d1f-e06d71408410.png",
+    link: "https://wh.workhub.ai/connect/chats",
+      code: "https://github.com/50ajmalkhan",
+  },
+{
+  title: "MultiVendor/ecommerce",
+    desc: "Developed a MultiVendor/ecommerce platform using React and Node.js, allowing multiple vendors to sell their products online and enabling customers to browse and purchase items from various vendors through a uniﬁed interface.",
+      tags: [
+        {
+          name: "React js",
+          color: "red-600",
+        },
+        {
+          name: "MaterialUI",
+          color: "green-600",
+        },
+        {
+          name: "ExpressJs",
+          color: "yellow-600",
+        },
+      ],
+        img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109035277814611978/Screenshot_19.png",
+            code: "https://github.com/50ajmalkhan/Flipcart-Ecommerce-Website",
+  },
+];
